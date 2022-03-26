e17f24aa8c2961763bbb98872d2b73466c84bc6c
Ugo Guazelli <guazelli.ugo@gmail.com>
2022-03-26 10:51:19 -0400

    added reset feature and room prefix

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 // Express JS
 const app = require("express")();
+const bodyParser = require("body-parser");
 // Socket IO
 const http = require("http").Server(app);
 const io = require("socket.io")(http, { cors: { origin: "*", methods: ["GET", "POST"] } });
 const registerRoomHandlers = require("./roomHandlers");
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 // Routes
 app.get("/", async (req, res, next) => {
 	return res.json({ message: "Welcome" });
+});
+
+app.post("/lastnumber", async (req, res, next) => {
+	const roomId = req.body.roomId;
+	const numberGenerated = req.body.numberGenerated;
+	emitToRoomAdmin(roomId, numberGenerated);
+	return res.json({ ok: true });
 });
 
 app.get("/company/:companySlug/room/:roomUniqueName", (req, res, next) =>
@@ -26,6 +40,11 @@ io.on("connection_error", (err) => {
 	console.log(err.context); // some additional error context
 });
 
+// Emit to Room Admin
+const emitToRoomAdmin = (roomId, numberGenerated) => {
+	const roomAdminName = `room_admin_${roomId}`;
+	io.emit(roomAdminName, numberGenerated);
+};
 // Start App
 const PORT = process.env.PORT || 9000;
 http.listen(PORT, function () {
